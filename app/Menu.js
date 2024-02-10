7ae1236fd58eea5780dbdc94c45661fb284d352b
swaggp <mutebinuhu1@gmail.com>
2024-02-10 12:16:02 +0300

    core: modify menu

--- app/Menu.js
+++ app/Menu.js
@@ -28,7 +28,7 @@ const productsList = [
       productName: 'Meat and All Foods',
       productImage:"https://img.freepik.com/premium-photo/bowl-goulash-soup-with-meat-potatoes-chillies-parsley_341862-1969.jpg?w=360",
       productDescription: 'Have Your Lunch with Our smoked First Meat, Feel  the aroma for every bite You take. You can have it with matooke, sweet potatoes, Cassava, White or brown rice',
-      price: 6000,
+      price: 8000,
       category:"localFood"
     },
     {
@@ -43,7 +43,7 @@ const productsList = [
       productName: 'Gnuts and All Foods',
       productImage:"https://i.pinimg.com/564x/04/9e/f6/049ef61c4c699ebe32958c13e608c307.jpg",
       productDescription: 'Whether Plain or with Smoked Fish, Im sure that we shall sparkle your Taste Buds. You can have Gnuts with any type of food You would like.',
-      price: 5000,
+      price: 7000,
       category:"localFood"
 
     },
@@ -51,7 +51,7 @@ const productsList = [
       productName: 'Beans and All Foods',
       productImage:"https://img.freepik.com/free-photo/vertical-view-tomato-soup-beans-lemon-cutting-board-mixed-color-background_140725-91982.jpg?w=360&t=st=1707402199~exp=1707402799~hmac=958106ac18fa28a099f22272a18d22c5c192174b3e5028ea1cc10923408dd278",
       productDescription: 'If You are a fun of Beans, You got covered. Do You want Some Beef In Beans still we gat You. Place Your order now with all Foods',
-      price: 5000,
+      price: 7000,
       category:"localFood"
 
     },
@@ -75,7 +75,7 @@ const productsList = [
       productName: 'Chips and Chicken',
       productImage:"/images/chips.jpg",
       productDescription: "Savor our irresistible duo of tender, expertly seasoned crispy chicken and perfectly golden chips. Each bite promises a delightful crunch, delivering a mouthwatering experience of flavors and texture",
-      price: 1000,
+      price: 10000,
       category:"snack"
 
     },
