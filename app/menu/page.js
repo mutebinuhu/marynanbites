e4fc224dc97f7b5575967b5fd8c35f335924d00f
swaggp <mutebinuhu1@gmail.com>
2024-03-31 14:32:51 +0400

    chore: add menu title

--- app/menu/page.js
+++ app/menu/page.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Menu from '../Menu';
 
+export const metadata = {
+    title: "Menu",
+    description: "Have a look at our menu and Place Your order now.",
+  };
 const Page = () => {
     return (
         <div className='bg-[#FFF8EE]'>
